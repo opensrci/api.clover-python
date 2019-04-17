f5920ed105ad59ae24d92b44feb771d1a15b126e
EC2 Default User <ec2-user@ip-172-31-25-139.ec2.internal>
2019-04-17 18:16:16 +0000

    working version 1.0

---
+++ sample.py
@@ -0,0 +1,56 @@
+'''
+MIT License
+
+Copyright (c) 2019 OpenSRCi
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+''' 
+
+import requests
+#import requests.exceptions
+import json
+import config
+
+headers={   
+            'Content-Type':'application/json',
+            'authorization':'Bearer '+ config.API_TOKEN
+}
+
+data = { "expend":"", "access_token" : config.API_TOKEN  }
+
+try:
+    myResponse = requests.get( config.url, data=data, headers= headers )
+    
+    myResponse.raise_for_status()
+    # Loading the response data into a dict variable
+    # json.loads takes in only binary or string variables so using content to fetch binary content
+    # Loads (Load String) takes a Json file and converts into python data structure (dict or list, depending on JSON)
+    parsed = json.loads(myResponse.text)
+    print(json.dumps(parsed, indent=4, sort_keys=True))
+
+except requests.exceptions.HTTPError as errh:
+    print ("Http Error:",errh)
+except requests.exceptions.ConnectionError as errc:
+    print ("Error Connecting:",errc)
+except requests.exceptions.Timeout as errt:
+    print ("Timeout Error:",errt)
+except requests.exceptions.RequestException as err:
+    print ("OOps: Something Else",err)
+    
+
